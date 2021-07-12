93884f2732ba485d645a07408a2433ce474aa927
emmanuelparisi <emmanuel.p2606@gmail.com>
2021-07-12 16:00:52 +0200

    add SearchBar

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,79 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <header>
+      <SearchBar @searchKeyUp="searchForElement"/>
+    </header>
+    <main>
+
+    </main>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import SearchBar from '@/components/SearchBar.vue'
+import axios from 'axios'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld
+    SearchBar
+  },
+  data(){
+    return {
+      apiLinkSearchMovies :"https://api.themoviedb.org/3/search/movie",
+      api_key : '7e7faac1db87ee1385f5258c9fdad986',
+      query : 'matrix'
+    }
+  },
+  created(){
+    this.library();
+  },
+  // computed : {
+  //   searchForElement(){
+  //     return this.query +='1';
+  //   }
+  // },
+  methods: {
+
+    library(){
+      axios
+      .get(this.apiLinkSearchMovies, {
+        params : {
+          api_key : this.api_key,
+          query : this.query
+        }
+      })
+      .then(rensponse=>{
+        console.log(rensponse.data);
+      })
+      .catch(function (error) {
+          console.log(error);
+      });
+    },
+
+    searchForElement(ele){
+      console.log(ele);
+      this.query = ele
+      this.library()
+      console.log(this.query);
+    }
+
   }
 }
 </script>
 
 <style lang="scss">
+@import '@/style/commons.scss';
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  // font-family: Avenir, Helvetica, Arial, sans-serif;
+  // -webkit-font-smoothing: antialiased;
+  // -moz-osx-font-smoothing: grayscale;
+  // text-align: center;
+  // color: #2c3e50;
+  // margin-top: 60px;
 }
 </style>
---
+++ src/components/SearchBar.vue
@@ -0,0 +1,23 @@
+<template>
+  <form>
+      <label for="searchbar">cerca</label>
+      <input type="text" id="searchbar" v-model="searchMovie" @keyup.prevent="$emit('searchKeyUp', searchMovie)">
+      <button>Cerca</button>
+  </form>
+</template>
+
+<script>
+export default {
+    name : 'SearchBar',
+    data() {
+        return {
+            searchMovie : ''
+        }
+    },
+    
+}
+</script>
+
+<style>
+
+</style>
